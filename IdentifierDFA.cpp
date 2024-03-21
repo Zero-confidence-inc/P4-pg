43a7f9ce15f04f940774499cc99ef7e5b61432c8
AAUMadsPoulsen <113096777+AAUMadsPoulsen@users.noreply.github.com>
2024-03-21 15:54:34 +0100

    Update IdentifierDFA.cpp (Needs testing)

--- IdentifierDFA.cpp
+++ IdentifierDFA.cpp
@@ -1,7 +1,32 @@
-#include "IdentifierDFA.h"
+#include <string>
+#include <vector>
+#include <Token.h>
 
-Token NumberDFA::tokenize(const std::string& input) {
-    Token token;
-    // DFA logic goes here
-    return token;
+class indentifierDFA{
+public:
+std::vector<Token> tokenize(const std::string& input) {
+    int DFA = 0;
+    std::vector<Token> tokens;
+    std::string currentToken = "";       
+    for(char c : input)
+    {
+        if(isalpha(c) and DFA == 0){
+            currentToken + c;
+            DFA = 1;
+        } else if (isalpha(c) and DFA == 1 or isalnum(c) and DFA == 1 or islink(c) and DFA == 1) {
+            currentToken + c;
+        } else
+        DFA = -1;
+    }
+    if (!currentToken.empty()){
+        tokens.push_back(Token(TokenType::IDENTIFIER, currentToken));
+    }
+    return tokens;
 }
+bool islink(char c)
+{
+    if (c == '_' or c == '-')
+        return true;
+    else return false;
+}
+};
