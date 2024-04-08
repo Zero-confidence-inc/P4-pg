fb99bda2a21014435953d2a48b013b164c5b74af
AAUMadsPoulsen <113096777+AAUMadsPoulsen@users.noreply.github.com>
2024-04-08 13:21:18 +0200

    Update IdentifierDFA.cpp

--- IdentifierDFA.cpp
+++ IdentifierDFA.cpp
@@ -4,7 +4,7 @@ IdentiferDFA::IdentifierDFA() : currentState(State::Start) {}
 bool IdentifierDFA::processChar(char c) {
     //Checks if character is in the alphabet and is the first character in the currentToken string
     //If it is, the character is added to the currentToken string
-    if((isalpha(c) and currentState = Start) or (islink(c) and currentState = Start)){
+    if((isalpha(c) and currentState == State:Start) or (islink(c) and currentState == State:Start)){
         currentToken += c;
         currentState = Working;
         return true;
@@ -12,7 +12,7 @@ bool IdentifierDFA::processChar(char c) {
         //Checks if the character is in the alphabet, is a number or is an acceptable linking symbol (see function further down)
         //Also checks if the state is "Working" indicating this is not the first character in the string. 
         //If this is true the character is added to the currentToken string.
-    else if ((isalpha(c) and currentState = Working) or (isalnum(c) and currentState = Working) or (islink(c) and currentState = Working)) {
+    else if ((isalpha(c) and currentState == State:Working) or (isalnum(c) and currentState == State:Working) or (islink(c) and currentState == State:Working)) {
         currentToken += c;
         return true;
     }
