33ada648dccb11f14e3b643de51dd8a5a318ac28
ZeroConfidence <amoldt22@student.aau.dk>
2024-04-15 02:33:00 +0200

    ConstDFA updated

--- ConstDFA.cpp
+++ ConstDFA.cpp
@@ -1,93 +1,87 @@
-#include <string>
-#include <vector>
-#include <stdio.h>
-#include <stdlib.h>
+#include "ConstDFA.h"
 #include <ctype.h>
-#include "Token.h"
 
-class ConstDFA {
-public:
-    std::vector<Token> tokenize(const std::string& input) {
-        std::vector<Token> tokens;
-        std::string currentToken;
-        std::string currentString;
-        State currentState = State::Start;
 
-        // Loops through each character in the input string (one charater at a time)
-        for (char c : input) {
-            switch (currentState){
-                case State::Start:
-                    // Checks for various single-character operators
-                    if ( isdigit(c)){
-                        currentString += c;
-                        currentToken = currentString;
-                        currentState = State::Hel;
-                    }else if (c=='-'){
-                    currentString += c;
-                    currentState = State::Minus;
-                    }
-                    break;
+ConstDFA::ConstDFA() : currentState(State::Start) {}
 
-                case State::Minus:
-                    if (isdigit(c)){
-                    currentString += c;
-                    currentToken = currentString;
-                    currentState = State::Hel;
-                    }
+bool ConstDFA::processChar(char c) {
+    switch (currentState){
+        case State::Start:
+            // Checks for various single-character operators
+            if ( isdigit(c)){
+                currentString += c;
+                currentToken = currentString;
+                currentState = State::IntegerPart;
+                return true;
+            }else if (c=='-'){
+                currentString += c;
+                currentState = State::Minus;
+                return true;
+            }
+            else return false;
+
+        case State::Minus:
+            if (isdigit(c)){
+            currentString += c;
+            currentToken = currentString;
+            currentState = State::IntegerPart;
+            return true;
+            }
+            else return false;
 
 
 
-                case State::Hel:
-                    // breaks afer complete operator is found
-                    if ( isdigit(c)){
-                       currentString += c;
-                       currentToken = currentString;
-                       currentState = State::Hel;
-                       }
-                    else if (c=','){
-                    currentString += c;
-                    currentState = State::Komma;
-                    }
-                    break;
+        case State::IntegerPart:
+            // breaks afer complete operator is found
+            if ( isdigit(c)){
+                currentString += c;
+                currentToken = currentString;
+                currentState = State::IntegerPart;
+                return true;
+                }
+            else if (c=','){
+            currentString += c;
+            currentState = State::Comma;
+            return true;
+            }
+            else return false;
 
-                case State::Komma:
-                    if (isdigit(c)){
-                        currentString += c;
-                        currentToken = currentString;
-                        currentState = State::EfterKomma;
-                    }
-                    break;
+        case State::Comma:
+            if (isdigit(c)){
+                currentString += c;
+                currentToken = currentString;
+                currentState = State::DecimalPart;
+                return true;
+            }
+            else return false;
 
 
-                // all states below here checks for double operators(also allows for their single counterpart to be used)
-                case State::EfterKomma:
-                    if (isdigit(c)){
-                        currentString += c;
-                        currentToken = currentString;
-                        currentState = State::EfterKomma;
-                    }
-                    break;
+    
+        case State::DecimalPart:
+            if (isdigit(c)){
+                currentString += c;
+                currentToken = currentString;
+                currentState = State::DecimalPart;
+                return true;
+            }
+            else return false;
             }
         }
-        // appends the last token if it is not empty
-        if (!currentToken.empty()){
-            tokens.push_back(Token(TokenType::CONST, currentToken));
-        }
-
-        return tokens;
-
-    }
-
-private:
-    // enum represents different states of the DFA for use in the switch case
-    enum class State{
-        Start,
-        Minus,
-        Hel,
-        Komma,
-        EfterKomma
+        
+Token ConstDFA::finalizeToken(){
+    Token token(TokenType::CONST, currentToken);
+    reset();
+    return token;
 
 
-    };
+}
 
-};
+bool ConstDFA::hasToken() const {
+    return !currentToken.empty();
+}
+
+void ConstDFA::reset() {
+    currentState = State::Start;
+    currentString.clear();
+    currentToken.clear();
+}    
--- ConstDFA.h
+++ ConstDFA.h
@@ -1,11 +1,29 @@
 #ifndef CONST_DFA_H
 #define CONST_DFA_H
 
+#include <string>
 #include "Token.h"
 
-class ConstDFA : public DFA{
+class ConstDFA : public DFA {
 public:
-    Token tokenize(const std::string& input);
+    ConstDFA();  // Constructor
+    bool processChar(char c) override;  // Process the next character
+    Token finalizeToken() override;  // Finalize and return the current token
+    bool hasToken() const override;  // Check if there is a current token
+    void reset() override;  // Reset the DFA to the initial state
+
+private:
+    enum class State {
+        Start,
+        Minus,
+        IntegerPart,
+        Comma,
+        DecimalPart
+    };
+
+    State currentState;
+    std::string currentToken;
+    std::string currentString;
 };
 
 #endif // CONST_DFA_H
