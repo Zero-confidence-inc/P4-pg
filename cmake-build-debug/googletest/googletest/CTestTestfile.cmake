af36ac91dba8ec9faf1d20c0f0ce5c37d7135852
ZeroConfidence <amoldt22@student.aau.dk>
2024-04-22 07:58:24 +0200

    made test for keywords 10/10

---
+++ cmake-build-debug/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Yuki/CLionProjects/P4-pg/googletest/googletest
+# Build directory: C:/Users/Yuki/CLionProjects/P4-pg/cmake-build-debug/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
