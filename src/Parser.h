7c12779e8145d8fa9cd17d098e8cd1a2203d2ee6
CGAddict <mads@webhobby.net>
2024-05-01 11:27:31 +0200

    Operator Parser

---
+++ src/Parser.h
@@ -0,0 +1,64 @@
+//
+// Created by Yuki on 29/04/2024.
+//
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "Token.h"
+#include <vector>
+#include <memory>
+#include <iostream>
+
+// Base class for all AST nodes
+struct ASTNode {
+    virtual ~ASTNode() {}
+};
+
+// Node for declarations
+struct DeclarationNode : ASTNode {
+    std::string type;
+    std::string identifier;
+};
+
+// Node for function declarations
+struct FunctionNode : DeclarationNode {
+    std::vector<std::shared_ptr<ASTNode>> body;
+};
+
+// Node for variable declarations
+struct VariableNode : DeclarationNode {
+    // Additional properties can be added here
+};
+
+struct IfStatementNode : ASTNode {
+    std::shared_ptr<ASTNode> condition;  // Node representing the condition expression
+    std::shared_ptr<ASTNode> trueBranch;  // Node representing the statements to execute if the condition is true
+    std::shared_ptr<ASTNode> falseBranch;  // Node representing the statements to execute if the condition is false (optional)
+
+    IfStatementNode() : condition(nullptr), trueBranch(nullptr), falseBranch(nullptr) {}
+};
+
+struct OperatorNode : ASTNode {
+    std::string operatorType; 
+};
+
+
+class Parser {
+public:
+    explicit Parser(const std::vector<Token>& tokens);
+    void parseProgram();
+    std::shared_ptr<ASTNode> parseDeclaration();
+    std::shared_ptr<ASTNode> parseOperator();
+
+
+private:
+    std::vector<Token> tokens;
+    size_t pos = 0;
+
+    bool lookAhead(TokenType expectedType);
+    void match(TokenType expectedType);
+    void match(TokenType expectedType, const std::string& expectedValue);
+    std::vector<std::shared_ptr<ASTNode>> parseFunctionBody();  // Assume this function parses a function body
+};
+
+#endif // PARSER_H
