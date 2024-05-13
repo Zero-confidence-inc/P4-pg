0d989460a5c18fcbe6cfe5e1fe21ecfeb0834fe6
Casper Gramkov Boejen <cbojen22@student.aau.dk>
2024-05-13 17:19:38 +0200

    Fixed Cmake Shit and starting writing tests

---
+++ Test/ParserTests.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <parser.h>
+
+
+class ParserTest : public ::testing::Test{
+protected:
+    
+
+    void SetUp() override{
+
+    }
+};
+
+TEST_F(){
+    
+    ASSERT_EQ() << "Int was not parsed"
+};
