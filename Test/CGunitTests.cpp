16b40094e20d2bda7986ecde003bc68d21351e06
Casper Gramkov Boejen <cbojen22@student.aau.dk>
2024-05-20 14:40:57 +0200

    Made CGTest
    made the value test 1/x

--- Test/CGunitTests.cpp
+++ Test/CGunitTests.cpp
@@ -4,10 +4,19 @@
 #include <string>
 
 TEST(CGTEST,DeclarationTest){
-    auto DeclarationTestNode = std::make_shared<DeclarationNode>();
-    DeclarationTestNode->type = "int";
-    DeclarationTestNode->identifier = "x";
+    auto DTN_basicDeclaration = std::make_shared<DeclarationNode>();
+    DTN_basicDeclaration->type = "int";
+    DTN_basicDeclaration->identifier = "x";
     CodeGenerator codeGenerator;
-    std::string DTresault = codeGenerator.generateDeclartionCode(DeclarationTestNode);
+    std::string DTresault = codeGenerator.generateDeclartionCode(DTN_basicDeclaration);
     EXPECT_EQ(DTresault,"int x;");
+}
+TEST(CGTEST,ValueTest){
+    auto VTN_basicValue = std::make_shared<ValueNode>();
+    VTN_basicValue->type = "int";
+    VTN_basicValue->identifier = "x";
+    auto ITN_basicValue = std::make_shared<IntNode>();
+    ITN_basicValue->integer = 12;
+    VTN_basicValue->value = ITN_basicValue;
+    EXPECT_EQ(VTN_basicValue,"int x = 12;");
 }
